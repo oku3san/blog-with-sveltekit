586fe1dd526d29ee69ec6a39412aa82b35f391d1
oku3san <takaya0303@gmail.com>
2023-08-20 19:31:05 +0900

    Update

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,10 +1,8 @@
-import { getDetail } from "../../lib/microcms";
-import type { PageServerLoad } from "./$types";
+import { getDetail } from '$lib/microcms';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-    return await getDetail(
-        params.slug
-    );
+	return await getDetail(params.slug);
 };
 
 export const prerender = true;
